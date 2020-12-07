52aba3520343601d3d5ae6d57b24919624041519
kms20a <kms21@naver.com>
2020-12-07 23:23:41 +0900

    securityConfig 설정

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View를 리턴하겠다!!
 public class IndexController {
@@ -13,5 +15,36 @@ public class IndexController {
 		// 머스테치 기본폴더 src/main/resources/
 		// 뷰리졸버 설정 : templates(prefix), .mustache(suffix) 생략가능
 		return "index"; //src/main/resources/templates/index.mustache
+	}
+	
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "manager";
+	}
+	
+	//스프링시큐리티 해당주소가 낚아 채버림 - SecurityConfig 파일 생성 후 작동안함.
+	@GetMapping("/login")
+	public @ResponseBody String login() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	public @ResponseBody String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "회원가입 완료됨!";
 	}
 }
